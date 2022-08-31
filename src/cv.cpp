f4a78dc0ccc75c7bc1b7cecdb2a34d5ec4846f25
Italo Russo <italrr@gmail.com>
2022-08-31 03:20:45 -0400

    progress

--- src/cv.cpp
+++ src/cv.cpp
@@ -21,7 +21,7 @@
 	]
 	
     [def type
-        [mock [
+        [proto [
             a: 5,
             b: 10,
             c: 15
@@ -31,9 +31,9 @@
 */
 
 static const std::vector<std::string> RESERVED_WORDS = {
-	"nil", "set", "mock", "fn",
+	"nil", "set", "proto", "fn",
 	"ret", "skip", "stop",
-	"if", "do", "para"
+	"if", "do", "iter"
 };
 
 namespace ItemType {
@@ -42,7 +42,7 @@ namespace ItemType {
 		NUMBER,
 		STRING,
 		LIST,
-		MOCK,
+		PROTO,
 		FUNCTION,
 		INTERRUPT
 	};
@@ -238,7 +238,97 @@ struct StringType : Item {
 };
 
 struct Context;
-std::shared_ptr<Item> infer(const std::string &input, Context *ctx, Cursor *cursor);
+static std::shared_ptr<Item> infer(const std::string &input, Context *ctx, Cursor *cursor);
+static std::shared_ptr<Item> eval(const std::string &input, Context *ctx, Cursor *cursor);
+
+struct ListType : Item {
+	int n;
+	std::vector<std::shared_ptr<Item>> list;
+	ListType(){
+		type = ItemType::LIST;
+		n = 0;
+	}
+	void build(const std::vector<std::shared_ptr<Item>> &objects){
+		this->list = objects;
+		this->n = objects.size();
+	}
+	std::shared_ptr<Item> copy(){
+		return std::make_shared<Item>(Item());
+	}
+	std::string str() const {
+		switch(type){
+			case ItemType::LIST: {
+				std::string _params;
+				std::string buff = "";
+				for(int i = 0; i < n; ++i){
+					buff += list[i]->str();
+					if(i < n-1){
+						buff += " ";
+					}                
+				}
+				return "["+buff+"]";
+			} break;
+			case ItemType::NIL: {
+				return "nil";
+			} break;
+			default: 
+				std::exit(1);
+		}
+	}
+};
+
+
+struct FunctionType : Item {
+	std::function<std::shared_ptr<Item>(const std::vector<std::shared_ptr<Item>> &operands, Context *ctx, Cursor *cursor)> lambda;
+	std::vector<std::string> params;
+	std::string body;
+	FunctionType(){
+		this->type = ItemType::FUNCTION;
+		this->body = "";
+		// this->innerType = true;
+		this->lambda = [](const std::vector<std::shared_ptr<Item>> &operands, Context *ctx, Cursor *cursor){
+			return std::make_shared<Item>(Item());
+		};  
+	}
+	FunctionType(   const std::string &name,
+				const std::function<std::shared_ptr<Item>(const std::vector<std::shared_ptr<Item>> &operands, Context &ctx, Cursor &cursor)> &lambda,
+				const std::vector<std::string> &params = {}){
+
+				}
+	
+	void set(const std::string &body, const std::vector<std::string> &params = {}){
+		this->lambda = [body](const std::vector<std::shared_ptr<Item>> &operands, Context *ctx, Cursor *cursor){
+			return eval(body, ctx, cursor);
+		};    
+		this->body = body;
+		// this->innerType = false;
+		this->params = params;
+	}
+	
+	std::shared_ptr<Item> copy(){
+		return std::make_shared<Item>(Item());
+	}
+
+	std::string str() const {
+		switch(type){
+			case ItemType::FUNCTION: {
+				std::string _params;
+				for(int i = 0; i < params.size(); ++i){
+					_params += params[i];
+					if(i < params.size()-1){
+						_params += " ";
+					}
+				}
+				return "["+_params+"]["+body+"]";
+			} break;
+			case ItemType::NIL: {
+				return "nil";
+			} break;
+			default:
+				std::exit(1);
+		}
+	}
+};
 
 struct MockType : Item {
 	std::unordered_map<std::string, std::shared_ptr<Item>> items;
@@ -359,33 +449,38 @@ std::shared_ptr<Item> infer(const std::string &input, Context *ctx, Cursor *curs
         auto v = std::stod(input);
         result->write(&v, sizeof(v), ItemType::NUMBER);
         return result;
-    }
+    }else
     // string
     if(isString(input)){
         auto result = std::make_shared<StringType>(StringType());
         result->literal = input.substr(1, input.length() - 2);
         return result;        
-    }
-    // // variable
-    // if(v->type != ItemType::NIL){
-    //     return v;
-    // }else{
-    //     // consider it a list otherwise list
-    //     auto result = std::make_shared<List>(List());
-    //     auto tokens = parse(input, cursor);
-    //     std::vector<std::shared_ptr<Object>> list;
-    //     for(int i = 0; i < tokens.size(); ++i){
-    //         list.push_back(eval(tokens[i], ctx, cursor));
-    //     }
-    //     result->build(list);
-    //     return result;      
-    // }
+    }else{
+		auto var = ctx->find(input);
+		if(var->type != ItemType::NIL){
+			return var;
+		}else{
+			auto result = std::make_shared<ListType>(ListType());
+			auto tokens = parse(input);
+			std::vector<std::shared_ptr<Item>> list;
+			for(int i = 0; i < tokens.size(); ++i){
+				list.push_back(eval(tokens[i], ctx, cursor));
+			}
+			result->build(list);
+			return result;      
+		}
+	}
 
     return std::make_shared<Item>(Item());
 }
 
-static bool isKeyword(const std::string &word){
-
+static bool isReserved(const std::string &word){
+	for(unsigned i = 0; i < RESERVED_WORDS.size(); ++i){
+		if(word == RESERVED_WORDS[i]){
+			return true;
+		}
+	}
+	return false;
 }
 
 static std::shared_ptr<Item> eval(const std::string &input, Context *ctx, Cursor *cursor){
@@ -399,50 +494,105 @@ static std::shared_ptr<Item> eval(const std::string &input, Context *ctx, Cursor
         return eval(imp, ctx, cursor);
     }
 
-	if(imp == "nil"){
-		return std::make_shared<Item>(Item());
-	}else
-	// DECLARE VARIABLE IN CURRENT CONTEXT (unless context switching colons are used)
-	if(imp == "set"){
-		auto name =  tokens[1];
-		auto val = eval(tokens[2], ctx, cursor);
-		ctx->add(name, val);
-		return val;
-	}else
-	// MOCKTYPE OBJECT
-	if(imp == "mock"){
-		auto mock = std::make_shared<MockType>(MockType());
+	if(isReserved(imp)){
+		// NIL
+		if(imp == "nil"){
+			return std::make_shared<Item>(Item());
+		}else
+		// SET declares a variable in the curret scope
+		if(imp == "set"){
+			auto name =  tokens[1];
+			auto val = eval(tokens[2], ctx, cursor);
+			ctx->add(name, val);
+			return val;
+		}else
+		// MOCK builds a prototype item: the other first class citizen of canvas (other one being list)
+		if(imp == "mock"){
+			auto mock = std::make_shared<MockType>(MockType());
 
-		std::vector<std::string> names;
-		std::vector<std::shared_ptr<Item>> values;
+			std::vector<std::string> names;
+			std::vector<std::shared_ptr<Item>> values;
 
-		for(unsigned i = 1; i < tokens.size(); ++i){
-			auto &token = tokens[i];
+			for(unsigned i = 1; i < tokens.size(); ++i){
+				auto &token = tokens[i];
 
-			auto fc = token.find(":");
+				auto fc = token.find(":");
 
-			std::string varname = token.substr(0, fc);
-			std::string varvalue = token.substr(fc+1, token.length()-fc-1);
+				std::string varname = token.substr(0, fc);
+				std::string varvalue = token.substr(fc+1, token.length()-fc-1);
 
-			names.push_back(varname);
-			values.push_back(eval(varvalue, ctx, cursor));
+				names.push_back(varname);
+				values.push_back(eval(varvalue, ctx, cursor));
+			}
+
+			mock->populate(names, values, ctx, cursor);	
+
+			return mock;		
+		}else
+		// FUNCTION
+		if(imp == "fn"){
+            auto largs = tokens[1];
+            if(largs.length() > 1 && largs[0] == '[' && largs[largs.length()-1] == ']'){
+                largs = largs.substr(1, largs.length() - 2);
+            }
+            auto args = parse(largs);
+            auto fn = std::make_shared<FunctionType>(FunctionType());
+            fn->set(tokens[2], args);
+            return fn;
+		}else
+		// RET
+		if(imp == "ret"){
+
+		}else
+		// SKIP (skips current execution ie inside a loop, next iteration [do true [[std:print test][skip]]]))
+		if(imp == "skip"){
+
+		}else
+		// STOP (stops current execution [do true [[std:print test][stop]]])
+		if(imp == "stop"){
+
+		}else
+		// IF conditional statement ([if [cond] [branch-true][branch-false]])
+		if(imp == "if"){
+
+		}else
+		// DO loop procedure ([do [cond] [code]...[code]])
+		if(imp == "do"){
+
+		}else
+		// ITER goes through a list ([iter [1 2 3 4] [code]...[code]]) ([iter [proto a:1 b:2 c:3] [code]...[code]])
+		if(imp == "iter"){
+
 		}
+	}else{
+		// Is it a variable?
+		auto var = ctx->find(imp);
+		if(var->type == ItemType::FUNCTION){
 
-		mock->populate(names, values, ctx, cursor);	
+			Context lctx(ctx);
+			auto func = static_cast<FunctionType*>(var.get());
+			bool useParams = func->params.size() > 0;
+			if(useParams && func->params.size() != tokens.size()-1){
+				std::cout << "fn '" << imp << "': expected [" << func->params.size() << "] arguments, received [" << tokens.size()-1 << "]" << std::endl;
+				return std::make_shared<Item>(Item());
+			}
+			std::vector<std::shared_ptr<Item>> operands;
+			for(int i = 1; i < tokens.size(); ++i){
+				auto ev = eval(tokens[i], &lctx, cursor);
+				// ev->name = std::to_string(i-1);
+				if(useParams){
+					// ev->name = func->params[i-1];
+				}
+				lctx.add(func->params[i-1], ev);
+				operands.push_back(ev);
+			} 
+			return func->lambda(operands, &lctx, cursor);  
 
-		return mock;		
+		// Let infer take care of the rest otherwise
+		}else{
+			return infer(input, ctx, cursor);
+		}
 	}
-	// FOR / WHILE / ITERATE
-	if(imp == "iter"){
-		// first argument is the conditional
-		// second argument is the function
-		
-
-	}else
-	if(imp == "for"){
-
-	}
-
 
 	return std::make_shared<Item>(Item());
 
@@ -464,8 +614,18 @@ int main(int argc, char* argv[]){
 
 	Cursor cursor;
 	Context ctx;
-	std::cout << eval("MOCK a:4 b:5", &ctx, &cursor)->str() << std::endl;
+	// std::cout << eval("MOCK a:4 b:5", &ctx, &cursor)->str() << std::endl;
+	// std::cout << eval("set a 5", &ctx, &cursor)->str() << std::endl;
 
+
+	while(true){
+		std::cout << std::endl;
+		std::string input;
+		std::cout << "> ";
+		std::getline (std::cin, input);
+		// std::cout << std::endl;
+		std::cout << eval(input, &ctx, &cursor)->str() << std::endl;
+	}
 
 
 }
